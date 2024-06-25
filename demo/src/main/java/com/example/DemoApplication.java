40730f203f9db049616f7912497456f275ffd413
lpardosu <lpardosu@LES010698.mshome.net>
2024-06-25 09:48:35 +0200

    Updated DTO

--- demo/src/main/java/com/example/DemoApplication.java
+++ demo/src/main/java/com/example/DemoApplication.java
@@ -69,10 +69,12 @@ public class DemoApplication implements CommandLineRunner {
 //		} else {
 //			actor.getErrors().forEach(System.out::println);
 //		}
+		
+//		var actor = new ActorDTO(0, "FROM", "DTO");
+//		dao.save(ActorDTO.from(actor));
 		dao.findAll().forEach(item -> System.out.println(ActorDTO.from(item)));
-		var actor = new ActorDTO(0, "FROM", "DTO");
-		dao.save(ActorDTO.from(actor));
-		dao.findAll().forEach(item -> System.out.println(ActorDTO.from(item)));
+		dao.readByActorIdGreaterThanEqual(200).forEach(System.out::println);
+		dao.queryByActorIdGreaterThanEqual(200).forEach(item -> System.out.println(item.getId() + " " + item.getNombre()));
 	}
 
 	/*
