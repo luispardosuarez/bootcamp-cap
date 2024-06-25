b1bb12184b84119cf59b45cdf23c45fffb29fc9d
lpardosu <lpardosu@LES010698.mshome.net>
2024-06-25 12:34:43 +0200

    Added ActorService to Service package

---
+++ demo/src/main/java/com/example/domains/contracts/services/ActorService.java
@@ -0,0 +1,7 @@
+package com.example.domains.contracts.services;
+
+import com.example.domains.core.contracts.services.ProjectionDomainService;
+
+public interface ActorService extends ProjectionDomainService<E, K> {
+
+}
