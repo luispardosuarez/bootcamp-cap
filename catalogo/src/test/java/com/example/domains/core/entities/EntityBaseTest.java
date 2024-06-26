7d0474e15dde3cd0f4becbea115070dc947d0ff6
lpardosu <lpardosu@LES010698.mshome.net>
2024-06-26 13:16:59 +0200

    Created package and file test

---
+++ catalogo/src/test/java/com/example/domains/core/entities/EntityBaseTest.java
@@ -0,0 +1,5 @@
+package com.example.domains.core.entities;
+
+public class EntityBaseTest {
+
+}
---
+++ catalogo/src/test/java/com/example/domains/core/validations/CadenasValidatorTest.java
@@ -0,0 +1,5 @@
+package com.example.domains.core.validations;
+
+public class CadenasValidatorTest {
+
+}
